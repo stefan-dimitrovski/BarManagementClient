f232f3a98f9e0c85e37363e2231b8af3b945e975
Stefan Dimitrovski <stefan.dimitrovski_19@hotmail.com>
2022-04-01 12:39:56 +0200

    bootstrap; primeng styles;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-    title = 'BarManagmentClient';
+export class AppComponent implements OnInit {
+    title = 'Bar Managment Client';
+
+    constructor(private primengConfig: PrimeNGConfig) {}
+
+    ngOnInit() {
+        this.primengConfig.ripple = true;
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonModule } from 'primeng/button';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, ButtonModule],
+    imports: [BrowserModule, AppRoutingModule, ButtonModule, NgbModule],
     providers: [],
     bootstrap: [AppComponent],
 })
