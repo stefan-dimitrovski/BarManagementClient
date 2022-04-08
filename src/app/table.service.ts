3e89029b92389f954243254920c28a16305f2872
stefan <stefan.dimitrovski_19@hotmail.com>
2022-04-08 19:36:47 +0200

    fixed typo; active link styles; router links;

--- src/app/table.service.ts
+++ src/app/table.service.ts
@@ -1,21 +1,22 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
-import { Table } from './domain/table';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {environment} from 'src/environments/environment';
+import {Table} from './domain/table';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class TableService {
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) {
+    }
 
-  getTables(): Observable<Table[]> {
-    return this.http.get<Table[]>(`${environment.server}/tables`)
-  }
+    getTables(): Observable<Table[]> {
+        return this.http.get<Table[]>(`${environment.server}/tables`)
+    }
 
-  getTableById(id: number): Observable<Table> {
-    return this.http.get<Table>(`${environment.server}/tables/${id}`)
-  }
+    getTableById(id: number): Observable<Table> {
+        return this.http.get<Table>(`${environment.server}/tables/${id}`)
+    }
 }
