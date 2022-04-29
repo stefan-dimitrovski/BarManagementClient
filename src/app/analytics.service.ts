086f7a05673856a141cd39f056bf0895e48aa667
Stefan Dimitrovski <stefan.dimitrovski_19@hotmail.com>
2022-04-29 11:36:30 +0200

    fixed analytics service and component;

--- src/app/analytics.service.ts
+++ src/app/analytics.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable, tap} from "rxjs";
+import {Observable} from "rxjs";
 import {environment} from "../environments/environment";
 import {EmployeesInLocalesAnalytics} from "./domain/employees-in-locales-analytics";
 
@@ -12,12 +12,7 @@ export class AnalyticsService {
     constructor(private http: HttpClient) {
     }
 
-    // getEmployeeNumbersByLocale(): Observable<EmployeesInLocalesAnalytics[]> {
-    //     return this.http.get<EmployeesInLocalesAnalytics[]>(`${environment.server}/analytics/employees-in-locales`)
-    // }
-    getEmployeeNumbersByLocale(): Observable<any> {
-        return this.http.get<any>(`${environment.server}/analytics/employees-in-locales`).pipe(
-            tap(value => console.log(value))
-        )
+    getEmployeeNumbersByLocale(): Observable<EmployeesInLocalesAnalytics[]> {
+        return this.http.get<EmployeesInLocalesAnalytics[]>(`${environment.server}/analytics/employees-in-locales`)
     }
 }
--- src/app/analytics/analytics.component.ts
+++ src/app/analytics/analytics.component.ts
@@ -21,50 +21,29 @@ export class AnalyticsComponent implements OnInit {
     ngOnInit(): void {
         this.analyticsService.getEmployeeNumbersByLocale().subscribe({
             next: value => {
-                // this.employeesInLocalesAnalytics = value;
-                // console.log(value);
-                // this.defineDatasets();
+                this.employeesInLocalesAnalytics = value;
+                this.defineDatasets();
             }
         });
     }
 
     defineDatasets() {
-        let datasets: any
-
-        // console.log(this.employeesInLocalesAnalytics);
+        let datasets: any[] = []
 
         this.employeesInLocalesAnalytics.forEach(e => {
-            // console.log(e.employeesByMonth.entries());
-            // datasets = [
-            //     {
-            //         label: e.locale.name,
-            //         data: e.employeesByMonth.entries(),
-            //         fill: false,
-            //         borderColor: '#FFA726',
-            //         tension: .4
-            //     },
-            // ]
+
+            datasets.push({
+                label: e.locale.name,
+                data: Object.values(e.employeesByMonth),
+                fill: false,
+                borderColor: datasets.length == 0 ? '#FFA726' : '#42A5F5',
+                tension: .4
+            })
         });
 
         this.employeeData = {
             labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
             datasets: datasets
-            // [
-            //     {
-            //         label: '1st Locale',
-            //         data: [65, 59, 80, 81, 56, 55, 40],
-            //         fill: false,
-            //         borderColor: '#FFA726',
-            //         tension: .4
-            //     },
-            //     {
-            //         label: '2nd Locale',
-            //         data: [28, 48, 40, 19, 86, 27, 90, 27, 86, 53, 32, 63],
-            //         fill: false,
-            //         borderColor: '#42A5F5',
-            //         tension: .4
-            //     }
-            // ]
         };
     }
 }
